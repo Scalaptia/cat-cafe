13bceb592ad48b05419f3ed40661fd2a08c32c32
Scalaptia <fernando@haroware.com>
2023-04-18 20:03:23 -0700

    initial config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+     {
+       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       type: 'asset/resource',
+     },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: './dist',
+    },
+    port: 8080,
+  },
+};
