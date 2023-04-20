3b2f400d4dee645ad80fd368bcac11a2ef52ae3a
Scalaptia <fernando@haroware.com>
2023-04-19 22:27:18 -0700

    hero logo

--- src/modules/hero.ts
+++ src/modules/hero.ts
@@ -1,8 +1,13 @@
+import Logo from '../assets/logo.jpg';
+import '../style/hero.css';
+
 export default function Hero() {
     const hero = document.createElement('div');
-    const logoImage = document.createElement('img');
+    hero.classList.add('hero-container')
 
-
+    const logoImage = new Image();
+    logoImage.src = Logo
+    logoImage.classList.add('logo')
 
     hero.appendChild(logoImage);
     
