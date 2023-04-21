e78069a26a7aa1079d1e5633f1c90e2cb30cb3bc
Scalaptia <fernando@haroware.com>
2023-04-21 16:30:45 -0700

    hero section

--- src/modules/hero.ts
+++ src/modules/hero.ts
@@ -5,13 +5,29 @@ export default function Hero() {
     const hero = document.createElement('div');
     hero.classList.add('hero-container')
 
-    const logoImage = new Image();
-    logoImage.src = Logo;
-    logoImage.classList.add('logo');
-    hero.appendChild(logoImage);
+    const heroLeft = document.createElement('div');
+    heroLeft.classList.add('hero-left');
 
-    const heroText = document.createElement('div');
-    heroText
+        const logoImage = new Image();
+        logoImage.src = Logo;
+        logoImage.classList.add('logo');
+        heroLeft.appendChild(logoImage);
+
+    const heroRight = document.createElement('div');
+    heroRight.classList.add('hero-right');
+
+        const heroTitle = document.createElement('h1');
+        heroTitle.classList.add('hero-title');
+        heroTitle.innerText = 'Welcome to Meow Café!'
+        heroRight.appendChild(heroTitle)
+
+        const heroText = document.createElement('h2');
+        heroText.classList.add('hero-text');
+        heroText.innerText = 'Escape to a world of purrs and relaxation at our cat cafe.';
+        heroRight.appendChild(heroText);
     
+        hero.appendChild(heroLeft);
+        hero.appendChild(heroRight);
+
     return hero
 }
