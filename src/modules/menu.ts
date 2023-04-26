ac2d65eff1323e28bae9352d6079112d53e1244c
Scalaptia <fernando@haroware.com>
2023-04-25 23:56:51 -0700

    hot coffee menu images

--- src/modules/menu.ts
+++ src/modules/menu.ts
@@ -1,8 +1,18 @@
 import '../style/menu.css';
 
-const createItem = (name:string, price:number) => {
+function getImgUrl(fileName: string){
+    const imgUrl = new URL(`../assets/menu-items/${fileName}.png`, import.meta.url).href
+    return imgUrl
+}
+
+const createItem = (name:string, price:number, imageUrl: string) => {
     const itemContainer = document.createElement('div');
     itemContainer.classList.add('item-container');
+
+        const itemImage = document.createElement('img');
+        itemImage.classList.add('item-image');
+        itemImage.src = imageUrl
+        itemContainer.appendChild(itemImage)
 
         const itemName = document.createElement('h3');
         itemName.classList.add('item-name');
@@ -32,14 +42,13 @@ export default function Menu() {
 
     const warmMenu = document.createElement('div');
     warmMenu.classList.add('warm-menu', 'menu');
-        warmMenu.appendChild(createItem('Espresso', 3.5));
-        warmMenu.appendChild(createItem('Cappuccino', 4));
-        warmMenu.appendChild(createItem('Latte', 4));
-        warmMenu.appendChild(createItem('Americano', 3.75));
-        warmMenu.appendChild(createItem('Coffee', 3));
-        warmMenu.appendChild(createItem('Hot Tea', 2.5));
-        warmMenu.appendChild(createItem('Chai Latte', 4));
-        warmMenu.appendChild(createItem('Hot Chocolate', 4));
+        warmMenu.appendChild(createItem('Espresso', 3.5, getImgUrl('espresso')));
+        warmMenu.appendChild(createItem('Cappuccino', 4, getImgUrl('capuccino')));
+        warmMenu.appendChild(createItem('Latte', 4, getImgUrl('latte')));
+        warmMenu.appendChild(createItem('Americano', 3.75, getImgUrl('americano')));
+        warmMenu.appendChild(createItem('Hot Tea', 2.5, getImgUrl('hot_tea')));
+        warmMenu.appendChild(createItem('Chai Latte', 4, getImgUrl('chai_latte')));
+        warmMenu.appendChild(createItem('Hot Chocolate', 4, getImgUrl('hot_chocolate')));
     container.appendChild(warmMenu);
 
     const coldMenuTitle = document.createElement('h3');
@@ -48,14 +57,11 @@ export default function Menu() {
 
     const coldMenu = document.createElement('div');
     coldMenu.classList.add('cold-menu', 'menu');
-        coldMenu.appendChild(createItem('Iced Tea', 3));
-        coldMenu.appendChild(createItem('Iced Mocha', 5));
-        coldMenu.appendChild(createItem('Iced Chai', 4.5));
-        coldMenu.appendChild(createItem('Iced Latte', 4.5));
-        coldMenu.appendChild(createItem('Iced Coffee', 3.25));
-        coldMenu.appendChild(createItem('Coffee Frappe', 5.75));
-        coldMenu.appendChild(createItem('Mocha Frappe', 6.25));
-        coldMenu.appendChild(createItem('Caramel Frappe', 6.25));
+        coldMenu.appendChild(createItem('Iced Tea', 3, getImgUrl('iced_tea')));
+        coldMenu.appendChild(createItem('Iced Mocha', 5, getImgUrl('iced_mocha')));
+        coldMenu.appendChild(createItem('Iced Chai', 4.5, getImgUrl('iced_chai')));
+        coldMenu.appendChild(createItem('Mocha Frappe', 6.25, getImgUrl('mocha_frappe')));
+        coldMenu.appendChild(createItem('Caramel Frappe', 6.25, getImgUrl('caramel_frappe')));
     container.appendChild(coldMenu);
 
     const foodMenuTitle = document.createElement('h3');
@@ -64,10 +70,10 @@ export default function Menu() {
 
     const foodMenu = document.createElement('div');
     foodMenu.classList.add('food-menu', 'menu');
-        foodMenu.appendChild(createItem('Pastries', 3.5));
-        foodMenu.appendChild(createItem('Sandwiches', 6));
-        foodMenu.appendChild(createItem('Salads', 6));
-        foodMenu.appendChild(createItem('Snack Trays', 6));
+        foodMenu.appendChild(createItem('Pastries', 3.5, getImgUrl('pastries')));
+        foodMenu.appendChild(createItem('Sandwiches', 6, getImgUrl('sandwiches')));
+        foodMenu.appendChild(createItem('Salads', 6, getImgUrl('salads')));
+        foodMenu.appendChild(createItem('Snack Trays', 6, getImgUrl('snack_trays')));
     container.appendChild(foodMenu);
 
     return container;
